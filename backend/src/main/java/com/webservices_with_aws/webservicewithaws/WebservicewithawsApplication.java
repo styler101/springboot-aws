c308670c627b7bc65b04f7dc3888f780772ba77c
Lucas <lucas.santos@mundowap.com.br>
2023-01-02 12:05:48 -0300

    chore: initial commit

---
+++ backend/src/main/java/com/webservices_with_aws/webservicewithaws/WebservicewithawsApplication.java
@@ -0,0 +1,13 @@
+package com.webservices_with_aws.webservicewithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicewithawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicewithawsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webservices_with_aws/webservicewithaws/WebservicewithawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices_with_aws.webservicewithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicewithawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
