93c49e8ae0fbf1ea3a30f3d94eda67e6da3b83d7
Lucas <lucas.santos@mundowap.com.br>
2023-01-02 21:08:59 -0300

    refactor: delete static folder and change application.properties to a yml file

---
+++ backend/src/main/java/com/webservices_with_aws/webservicewithaws/SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.webservices_with_aws.webservicewithaws;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
