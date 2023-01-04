8c32a3997f31e2f27d8b6a5d99341def5b6bd00f
Lucas <lucas.santos@mundowap.com.br>
2023-01-04 15:02:38 -0300

    chore: add multiply method

--- backend/src/main/java/com/webservices_with_aws/webservicewithaws/MathController.java
+++ backend/src/main/java/com/webservices_with_aws/webservicewithaws/MathController.java
@@ -9,10 +9,10 @@ import java.util.Locale;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-
+@RequestMapping(value="/operations")
 public class MathController {
 
-   @RequestMapping(value="/sum/{number1}/{number2}", method = RequestMethod.GET)
+   @GetMapping(value="/sum/{number1}/{number2}")
    public double sum(@PathVariable(value = "number1") String numberOne,  @PathVariable(value="number2") String numberTwo) throws UnsupportedMathOperationException {
       if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
          // devemos sempre retornar a classe de erro.
@@ -21,7 +21,7 @@ public class MathController {
       return convertToDouble(numberOne) + convertToDouble(numberTwo);
    }
 
-   @RequestMapping(value="/sub/{number1}/{number2}", method = RequestMethod.GET)
+   @GetMapping(value="/sub/{number1}/{number2}")
    // Vamos receber um parametros de rota são passados como string
    public double sub(@PathVariable(value = "number1") String number1, @PathVariable(value = "number2") String number2) throws UnsupportedMathOperationException{
       if(!isNumeric(number1) || !isNumeric(number2)) {
@@ -30,6 +30,14 @@ public class MathController {
       return convertToDouble(number1) - convertToDouble(number2);
    }
 
+
+   @GetMapping(value="/multiply/{number1}/{number2}")
+   public double multiply(@PathVariable(value="number1") String number1, @PathVariable(value="number2") String number2) throws UnsupportedMathOperationException{
+      if(!isNumeric(number1) || !isNumeric(number2)){
+         throw new UnsupportedMathOperationException("Please set a number on request param.");
+      }
+      return convertToDouble(number1) * convertToDouble(number2);
+   }
 
    private boolean isNumeric(String stringNumber){
       // verifca se o número é positivo ou negaitvo de 1 a 9 e possui casas decimais
